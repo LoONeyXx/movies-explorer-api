e9c5cc1518b8421c2cfa5e5511c783d539a36713
Sergey Gromov <looneyxx@yandex.ru>
2023-08-23 14:03:57 +0300

    добавил dotenv и logger

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,9 @@
-export const { PORT = 4000, BASE_URL = 'mongodb://127.0.0.1:27017/moviesdb', JWT_SECRET = 'daskdljkj3141231wlkjdeas' } = process.env;
+import dotenv from 'dotenv';
+
+dotenv.config();
 export const pattern = /(http[s]?:\/\/)(?:www)?[A-Za-zа-яА-яё0-9-._~:/?#[\]@!$&'()*+,;=]+\.\w{2,4}(?:\/[A-Za-zа-яА-яё0-9-._~:/?#[\]@!$&'()*+,;=]+)?(?:#$)?/;
+
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const PORT = 3000;
+export const DB_CONN = process.env.DB_CONN || 'mongodb://127.0.0.1:27017/moveiesdb';
+export const JWT_SECRET = process.env.JWT_SECRET || 'super-strong-secret';
