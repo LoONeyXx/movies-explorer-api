eee66dad8f30658d65fe769380e5548da99613c2
Sergey Gromov <looneyxx@yandex.ru>
2023-08-23 14:06:27 +0300

    Исправил название базы данных

--- utils/config.js
+++ utils/config.js
@@ -5,5 +5,5 @@ export const pattern = /(http[s]?:\/\/)(?:www)?[A-Za-zа-яА-яё0-9-._~:/?#[\]
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = 3000;
-export const DB_CONN = process.env.DB_CONN || 'mongodb://127.0.0.1:27017/moveiesdb';
+export const DB_CONN = process.env.DB_CONN || 'mongodb://127.0.0.1:27017/moviesdb';
 export const JWT_SECRET = process.env.JWT_SECRET || 'super-strong-secret';
