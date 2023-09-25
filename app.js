e7ffa10d9cb94c85f0a1c97fd8ddc69fdce54359
Sergey Gromov <looneyxx@yandex.ru>
2023-09-25 14:13:22 +0300

    check cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(helmet());
 dotenv.config();
 app.use(cookieParser());
 app.disable('x-powered-by');
-app.use(cors({ origin: ['http://localhost:3000', 'https://moovielooney.nomoredomainsrocks.ru'], credentials: true }));
+app.use(cors({ origin: 'https://moovielooney.nomoredomainsrocks.ru', credentials: true }));
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(router);
